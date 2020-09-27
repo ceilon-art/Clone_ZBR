56f2a2aa2732fc46ab0f723c9f69b8e36f32e404
Andre Luiz <andreluizkanaan222@gamil.com>
2020-09-27 11:06:27 -0300

    Final Commit

--- src/components/Informations/styles.ts
+++ src/components/Informations/styles.ts
@@ -3,7 +3,6 @@ import styled from "styled-components/native";
 export const Container = styled.View`
   flex: 1;
   flex-direction: row;
-  /* background: #87cefa; */
 
   max-height: 95%;
 `;
--- src/services/api.ts
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-const api = axios.create({
-  baseURL: "http://www.zbr.net.br/",
-});
-
-export default api;
